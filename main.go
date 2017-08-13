8c97b30df33296f35fafaaed88026659e2153139
Rafael Cunha de Almeida <rafael.almeida@ef.com>
2017-08-13 17:30:30 +0100

    Tree serialization

---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+)
+
+const (
+	GoLeft  = iota
+	GoRight
+	Return
+	Read
+)
+
+type BinaryTree struct {
+	Value int
+	Left  *BinaryTree
+	Right *BinaryTree
+}
+
+func (b *BinaryTree) String() string {
+	result := ""
+	if b.Left != nil {
+		result = b.Left.String() + " "
+	}
+	result += fmt.Sprintf("%d", b.Value)
+	if b.Right != nil {
+		result += " " + b.Right.String()
+	}
+	return result
+}
+
+func deserialize(ptr int, data []int) (int, *BinaryTree) {
+	tree := new(BinaryTree)
+
+	for ptr < len(data) {
+		switch cmd := data[ptr]; cmd {
+		case GoLeft:
+			ptr, tree.Left = deserialize(ptr+1, data)
+		case GoRight:
+			ptr, tree.Right = deserialize(ptr+1, data)
+		case Read:
+			ptr++
+			tree.Value = data[ptr]
+			ptr++
+		case Return:
+			ptr++
+			return ptr, tree
+		}
+	}
+
+	return ptr, tree
+}
+
+func Deserialize(data []int) *BinaryTree {
+	_, t := deserialize(0, data)
+	return t
+}
+
+func (b *BinaryTree) serialize(data *[]int) {
+	if b.Left != nil {
+		*data = append(*data, GoLeft)
+		b.Left.serialize(data)
+	}
+	*data = append(*data, Read)
+	*data = append(*data, b.Value)
+	if b.Right != nil {
+		*data = append(*data, GoRight)
+		b.Right.serialize(data)
+	}
+	*data = append(*data, Return)
+}
+
+// size complexity O(5*N), where N = number of nodes
+// time complexity O(N) (amortized)
+func (b *BinaryTree) Serialize() []int {
+	data := new([]int)
+	b.serialize(data)
+	return *data
+}
---
+++ main_test.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestToString(t *testing.T) {
+	tree := BinaryTree{
+		Value: 5,
+		Left: &BinaryTree{
+			Value: 3,
+			Left:  &BinaryTree{
+				Value: 2,
+				Left: &BinaryTree{Value: 1},
+			},
+			Right: &BinaryTree{Value: 4},
+		},
+		Right: &BinaryTree{Value: 6},
+	}
+	if tree.String() != "1 2 3 4 5 6" {
+		t.Errorf("expected: 1 2 3 4 5 6, got: %s", tree.String())
+	}
+}
+
+func TestDeserialize(t *testing.T) {
+	data := []int{
+		GoLeft,
+		GoLeft,
+		GoLeft,
+		Read,
+		1,
+		Return,
+		Read,
+		2,
+		Return,
+		Read,
+		3,
+		GoRight,
+		Read,
+		4,
+		Return,
+		Return,
+		Read,
+		5,
+		GoRight,
+		Read,
+		6,
+		Return,
+		Return,
+	}
+
+	output := Deserialize(data).String()
+
+	if output != "1 2 3 4 5 6" {
+		t.Errorf("expected: 1 2 3 4 5 6, got: %s", output)
+	}
+}
+
+func TestSerialize(t *testing.T) {
+	data := []int{
+		GoLeft,
+		GoLeft,
+		GoLeft,
+		Read,
+		1,
+		Return,
+		Read,
+		2,
+		Return,
+		Read,
+		3,
+		GoRight,
+		Read,
+		4,
+		Return,
+		Return,
+		Read,
+		5,
+		GoRight,
+		Read,
+		6,
+		Return,
+		Return,
+	}
+	tree := BinaryTree{
+		Value: 5,
+		Left: &BinaryTree{
+			Value: 3,
+			Left:  &BinaryTree{
+				Value: 2,
+				Left: &BinaryTree{Value: 1},
+			},
+			Right: &BinaryTree{Value: 4},
+		},
+		Right: &BinaryTree{Value: 6},
+	}
+
+	serialized := tree.Serialize()
+
+	if !reflect.DeepEqual(serialized, data) {
+		t.Errorf("expected: %v, got: %v", data, serialized)
+	}
+}
+
+func TestSerializeDeserialize(t *testing.T) {
+	tree := BinaryTree{
+		Value: 5,
+		Left: &BinaryTree{
+			Value: 3,
+			Left:  &BinaryTree{
+				Value: 2,
+				Left: &BinaryTree{Value: 1},
+			},
+			Right: &BinaryTree{Value: 4},
+		},
+		Right: &BinaryTree{Value: 6},
+	}
+
+	deserialized := Deserialize(tree.Serialize())
+
+	if tree.String() != deserialized.String() {
+		t.Errorf("expected: %s, got: %s", tree.String(), deserialized.String())
+	}
+}
